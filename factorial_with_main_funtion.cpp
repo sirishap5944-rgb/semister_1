3960f571b532c77cc0ffac1c6036e8e0107fa316
sirishap5944-rgb <sirishap5944@gmail.com>
2025-12-02 10:52:34 +0530

    Add files via upload

---
+++ factorial_with_main_funtion.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	unsigned long long fact=1;
+	printf("enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		fact*=i;
+	}
+	printf("factorial of %d\n = %llu",fact,n);
+	return 0;
+}
---
+++ int,char,float,double,long - Copy - Copy (2).cpp
@@ -0,0 +1,16 @@
+ //int,char,float,double,long
+ #include<stdio.h>
+ int main()
+ {
+ 	float b=5.5
+ 	character c(12)=76547;
+	doubled =5.234567891;
+ 	long int=3500650078;
+ 	printf("size of a is :%d\n",size of (a));
+ 	printf("size of b is :%d\n",size of (b));
+ 	printf("size of c is :%d\n",size of (c));
+ 	printf("size of d is :%d\n",size of (d));
+ 	printf("size of e is :%d\n",size of (e));
+ 	return 0;
+ }
+ 
