20a48e9ea2f8819e4d007513114d14862deeb503
sirishap5944-rgb <sirishap5944@gmail.com>
2025-12-02 11:34:04 +0530

    Add files via upload

---
+++ binarysearching.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int i,low,high,mid,n,found =0;arr[50];
+	printf("enter n value:");
+	scanf("%d",&n);
+	printf("enter array elements in sorted order");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",arr[i]);
+	}
+	printf("enter key value:");
+	scanf("%d",&key);
+	low = 0;
+	high = n-1;
+	while(low<=high)
+	{
+		mid =(low+high)/2;
+		if(key==mid)
+	{
+		printf("elements found at %d:",mid)
+	found =1;
+	break;
+}
+
+
+
+
+}
