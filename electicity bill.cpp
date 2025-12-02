85881692acd84b1bc6b3a15f728d1ff0c14e03bd
sirishap5944-rgb <sirishap5944@gmail.com>
2025-12-02 10:57:40 +0530

    Add files via upload

---
+++ electicity bill.cpp
@@ -0,0 +1,12 @@
+//electricity bill
+#include<stdio.h>
+int main()
+{
+	int units;
+	int price=4
+	printf("the no of units used is:");
+	scanf("%d",&units);
+	int total bill = units*price;
+	print("the amount of your bill is %d",& total bill);
+	return 0;
+}
---
+++ maximuminputofarrays.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a[10],max;
+	printf("enter n value");
+	scanf("%d",&n);
+	printf("enter array elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+	for(i=1;i<n;i++)
+	{
+		if(a[i]>max)
+		{
+			max = a[i];
+	    }
+			printf("\nmaximun element is:%d",max);
+		}
+			return 0;
+    }
+
+
+
+
+ 
+	
+	
+
